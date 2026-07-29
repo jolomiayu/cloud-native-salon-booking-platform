afad42918d1e15a982f7a1296cad4f2cb6b635d2
Jolomi Ayu <jolomianthonyayu@gmail.com>
2026-07-29 12:59:34 +0000

    Add frontend booking button functionality

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const bookBtn = document.getElementById("bookBtn");
+
+    bookBtn.addEventListener("click", () => {
+        alert("Book Appointment button clicked!");
+    });
+
+});

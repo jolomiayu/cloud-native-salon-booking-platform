c39fdcf3d9d4863f578006bc877ce97ed4c3b7a9
Jolomi Ayu <jolomianthonyayu@gmail.com>
2026-08-01 09:02:42 +0000

    Fix PostgresSQL schema and initialize RDS database

--- database/init.sql
+++ database/init.sql
@@ -1,15 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'customer'
+);
+
 CREATE TABLE IF NOT EXISTS bookings (
     id SERIAL PRIMARY KEY,
     customer VARCHAR(100) NOT NULL,
     service VARCHAR(100) NOT NULL,
     booking_date DATE NOT NULL,
-    booking_time TIME NOT NULL
+    booking_time TIME NOT NULL,
     user_id INTEGER REFERENCES users(id)
 );
 
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-    role VARCHAR(20) NOT NULL DEFAULT 'customer'
-);
